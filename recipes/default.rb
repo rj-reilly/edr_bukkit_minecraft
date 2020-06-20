cb617695f148888a1bd019998224293b63896d84
Robert Reilly <robertjreilly@gmail.com>
2020-06-20 12:48:59 -0400

    working spigot

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,30 +14,30 @@ remote_file "#{node['edr_bukkit_minecraft']['home']}/spigot-1.15.2.jar" do
   action :create
 end
 
-# cookbook_file "#{node['edr_bukkit_minecraft']['home']}/run.bat" do
-#   action :create
-# end
+cookbook_file "#{node['edr_bukkit_minecraft']['home']}/run.bat" do
+  action :create
+end
 
-# %w(banned-ips banned-players blacklist ops whitelist).each do |j|
-#   cookbook_file "#{node['edr_bukkit_minecraft']['home']}/#{j}.json" do
-#     source "#{j}.json"
-#     action :create
-#   end
-# end
+%w(banned-ips banned-players blacklist ops whitelist).each do |j|
+  cookbook_file "#{node['edr_bukkit_minecraft']['home']}/#{j}.json" do
+    source "#{j}.json"
+    action :create
+  end
+end
 
-# %w(bukkit commands help permissions).each do |y|
-#   cookbook_file "#{node['edr_bukkit_minecraft']['home']}/#{y}.yml" do
-#     source "#{y}.yml"
-#     action :create
-#   end
-# end
+%w(bukkit commands help permissions).each do |y|
+  cookbook_file "#{node['edr_bukkit_minecraft']['home']}/#{y}.yml" do
+    source "#{y}.yml"
+    action :create
+  end
+end
 
-# execute 'install bukkit' do
-#   command "java -Xmx2018m -jar #{node['edr_bukkit_minecraft']['home']}//craftbukkit-1.15.2.jar" 
-#   action :run
-# end
+execute 'install bukkit' do
+  command "java -Xmx2018m -jar #{node['edr_bukkit_minecraft']['home']}//craftbukkit-1.15.2.jar" 
+  action :run
+end
 
-# cookbook_file "#{node['edr_bukkit_minecraft']['home']}/eula.txt" do
-#   source 'eula.txt'
-#   action :create
-# end
+cookbook_file "#{node['edr_bukkit_minecraft']['home']}/eula.txt" do
+  source 'eula.txt'
+  action :create
+end
